54d197018395f10c10fedcacf4778d31a5816d86
Step X <stepan170297@gmail.com>
2023-11-24 22:11:34 +0400

    Ready

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,20 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.printf("Hello and welcome!");
-        
+        int startbalance = 100;
+        int payone = 1100;
+        int paytwo = 300;
+        boolean More500 = true;
 
-
-            System.out.println("i = " + i);
+        int bonus;
+        if (More500) {
+            bonus = payone / startbalance;
+        } else {
+            bonus = paytwo / startbalance;
         }
+
+        int balance = bonus + payone + startbalance;
+        System.out.println("Итоговая сумма" + balance);
     }
+
 }

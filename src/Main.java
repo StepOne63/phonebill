ff35d0557504bfb88e22ed362d80c83317163983
Step X <stepan170297@gmail.com>
2023-11-27 21:25:40 +0400

    FixPrice

--- src/Main.java
+++ src/Main.java
@@ -3,20 +3,20 @@ public class Main {
     public static void main(String[] args) {
 
         int startBalance = 100;
-        int replenishmentMore500 = 1100;
-        int replenishmentLess500 = 300;
+        int replenishmentMore1000 = 1100; // Платеж больше тысячи
+        int replenishmentLess1000 = 300; // Платеж меньше тысячи
 
         int bonus;
-        if (replenishmentMore500 > 500) {
-            bonus = replenishmentMore500 / startBalance;
+        if (replenishmentMore1000 > 1000) {
+            bonus = replenishmentMore1000 / startBalance;
         } else {
-            bonus = replenishmentLess500 / startBalance;
+            bonus = replenishmentLess1000 / startBalance;
         }
 
-        int balance = bonus + replenishmentMore500 + startBalance;
+        int balance = bonus + replenishmentMore1000 + startBalance;
         System.out.println("Итоговая сумма" + balance);
 
-        int amountBonus = replenishmentMore500 / startBalance;
+        int amountBonus = replenishmentMore1000 / startBalance;
         System.out.println("Полученные бонусы" + amountBonus);
     }
 
